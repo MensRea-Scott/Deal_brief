516af6bf2d413065587358c14fefa2a9097392cf
MensRea-Scott <yangzen_1@126.com>
2014-06-20 18:46:15 +0800

    added file_read

---
+++ excel_map.py
@@ -0,0 +1,15 @@
+#encoding: utf-8
+
+class excel_map:
+	Deal_owner = {'Category Manager':(0,2)}
+	Property = {'Licensed Properties':(1,2)}
+	Licensee = {'Licensee Name':(0,2)}
+	Comdate = {'Contract Start Date':(0,2)}
+	Enddate = {'Contract End Date':(0,2)}
+	#Category = []
+	#MG = {}
+	#Payment={}
+	Rytrate = {'Royalty Rate':(1,0)}
+	Selloff = {'Sell Off Date':(0,2)}
+
+	
